2b5eb7f9e2413fd1a6502a7998511955e1ecda98
Richárd Zloch <zloch.richard@verebelyszki.hu>
2021-10-12 09:39:09 +0200

    Add project files.

---
+++ CA211012/Kategoria.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CA211012
+{
+    class Kategoria
+    {
+        public string Nev { get; set; }
+        public int Eltunt  { get; set; }
+        public int Tulelo { get; set; }
+
+        public Kategoria(string r)
+        {
+            var t = r.Split(';');
+
+
+            Nev = t[0];
+            Eltunt = int.Parse(t[1]);
+            Tulelo = int.Parse(t[2]);
+        
+        }
+    }
+}
---
+++ CA211012/Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.IO;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CA211012
+{
+    class Program
+    {
+        static List<Kategoria> kategoriak = new List<Kategoria>();
+        static void Main(string[] args)
+        {
+            Beolvasas();
+            Console.WriteLine($"2. feladat: {kategoriak.Count} db");
+            Console.WriteLine($"3. feladat: {kategoriak.Sum(x => x.Eltunt + x.Tulelo)} fő");
+            Console.Write($"4. feladat: Kulcsszó: ");
+            string ksz = Console.ReadLine();
+            bool van = kategoriak.Any(x => x.Nev.Contains(ksz));
+            Console.WriteLine($"\t{(van ? "Van találat" : "Nincs találat")}");
+            if (van)
+            {
+                Console.Write("5. feladat: ");
+                kategoriak.Where(x => x.Nev.Contains(ksz))
+                    .ToList()
+                    .ForEach(y => Console.WriteLine($"\n{y.Nev} {y.Tulelo + y.Eltunt} fő"));
+            }
+
+            Console.WriteLine($"7. feladat: {kategoriak.Where(x => x.Tulelo == kategoriak.Max(y => y.Tulelo)).First().Nev}");
+               
+            Console.ReadKey();
+        }
+
+        private static void Beolvasas()
+        {
+            using (var sr = new StreamReader("titanic.txt", Encoding.UTF8))
+            {
+                while (!sr.EndOfStream)
+                {
+                    kategoriak.Add(new Kategoria(sr.ReadLine()));
+                }
+            }
+            
+
+        }
+    }
+}
